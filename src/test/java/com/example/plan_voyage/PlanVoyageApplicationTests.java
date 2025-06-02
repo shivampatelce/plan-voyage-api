3bc4f28b6e18e3be219b44a854c5f1bb48e5553a
shivampatelce <shivampatelce@gmail.com>
2025-06-02 16:04:54 -0400

    initial commit

---
+++ src/test/java/com/example/plan_voyage/PlanVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plan_voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
